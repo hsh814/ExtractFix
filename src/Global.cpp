00e32ea3eff7ccfe72b835941ff41941fbcf0338
Gao Xiang <gaoxiang@comp.nus.edu.sg>
2019-05-28 20:53:40 +0800

    fix the error when initializing string variable

--- src/Global.cpp
+++ src/Global.cpp
@@ -20,8 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 struct Config config = {
         /* verbose                  = */ false,
-        /*binaryPath                = */ nullptr,
-        /*binaryName                = */ nullptr
+        /*binaryPath                = */ "",
+        /*binaryName                = */ ""
 };
 
 int ERROR_EXIT_CODE = 1;
