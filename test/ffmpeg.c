ece93f53da9073696f6f44dea39c871a3cb7bfac
wangbo15 <wangbo_pku_15@163.com>
2019-06-13 21:31:04 +0800

    add symbolicInserter, tracer

--- test/ffmpeg.c
+++ test/ffmpeg.c
@@ -9,25 +9,25 @@
 
 static int decode_dds1(int segments, uint8_t *frame, int width, int height)
 {
+	klee_make_symbolic(&width, sizeof(width), "width");
 
     const uint8_t *frame_start = frame;
     const uint8_t *frame_end   = frame + width * height;
     int i, v, offset, count;
 	
-	
-	klee_make_symbolic(&width, sizeof(width), "width");
+
 	//klee_make_symbolic(&height, sizeof(height), "height");
-	klee_make_symbolic(&frame, sizeof frame, "frame");
-	klee_make_symbolic(&frame_end, sizeof frame_end, "frame_end");
+	//klee_make_symbolic(&frame, sizeof frame, "frame");
+	//klee_make_symbolic(&frame_end, sizeof frame_end, "frame_end");
 
 	if (frame_end - frame < width + 3)
 	    return AVERROR_INVALIDDATA;
 	
-	
+	//klee_make_symbolic(&width, sizeof(width), "width");
 	
 	frame += 2;
-	//frame[width] = '0';
-	frame[width + 1] =  '0';
+	klee_assume(frame + width + 1 < frame_end);
+	frame[width + 1] =  '0'; // width + 1 < 6
 	
 	frame += 2;
 	
@@ -42,7 +42,7 @@ void* x_malloc(int n){
 
 int main(){
 	int size = 6;
-	
+	//SIZE = size;
 	uint8_t* frame = (uint8_t*) x_malloc(size);
 	
   	//klee_make_symbolic(frame, sizeof(frame), "frame");
