86c6d5a0cbddaca60f343549806570162f1a861a
wangbo15 <wangbo_pku_15@163.com>
2019-08-14 18:56:15 +0800

    add libxml2-5969

---
+++ benchmark/libxml2-5969/project_config.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+compile_type=$1
+
+current_dir=`pwd`
+
+# get project and set to corresponding version
+# git clone https://gitlab.gnome.org/GNOME/libxml2/ project
+cd project
+#git checkout d9783e4
+
+# create build diretory and config
+rm -rf klee
+mkdir klee
+cd klee
+
+sed -i 's/#define FQUOTIENT(a,b)                  (floor(((double)a\/(double)b)))/double FQUOTIENT(double a, double b){return floor(((double)a\/(double)b));}/' ../xmlschemastypes.c
+
+cflags="-g -lkleeRuntest -lkleeBasic -Wno-everything"
+
+if [ $compile_type == 'to_bc' ];
+then
+    # required by wllvm
+    export LLVM_COMPILER=clang
+    compiler=wllvm
+elif [ $compile_type == 'lowfat' ];
+then
+    compiler=${LOWFAT_CLANG}
+    cflags="$cflags -fsanitize=lowfat -mllvm -lowfat-debug -mllvm -lowfat-null-deref -mllvm -lowfat-no-check-reads -mllvm -lowfat-no-check-writes -mllvm -lowfat-no-check-memset -mllvm -lowfat-no-check-memcpy -mllvm -lowfat-no-check-escapes -mllvm -lowfat-no-check-fields -mllvm -lowfat-check-whole-access -mllvm -lowfat-no-replace-globals -mllvm -lowfat-no-replace-alloca -mllvm -lowfat-no-replace-malloc -mllvm -lowfat-symbolize -lstlimpl"
+    #cflags="$cflags -fsanitize=address"
+fi
+
+CC=$compiler CFLAGS="$cflags" ../autogen.sh --enable-static # &> /dev/null
+
+cd ../..
+
+
+
+#$LOWFAT_CLANG -fsanitize=signed-integer-overflow,unsigned-integer-overflow -I./project/klee/include/ -I./project/include -L./project/klee/.libs/ -lxml2 -lstlimpl /home/nightwish/workspace/bug_repair/LowFat/lowfat.o poc.c -o poc
+
+#LD_PRELOAD=$LD_PRELOAD:./project/klee/.libs/libxml2.so ./poc
